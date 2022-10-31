e18f47fbae7b66d82afe6400baa354d7d7916dbf
Vijay-dev-web <85775060+Vijay-dev-web@users.noreply.github.com>
2022-10-31 14:03:05 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require('express')
 
 const app = express()
 
+app.get('/', () => {
+    console.log("Welcome to routes...!) 
+})
+
 app.listen(3000, () => {
   console.log("Server listening on 3000")
 })
