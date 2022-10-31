0170e9bbb3f8419c3927d16de942f9a3361bf37f
sahayavijay <sahayavijay1993@gmail.com>
2022-10-31 22:07:56 +0530

    added home page

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ app.get('/', () => {
     console.log("Welcome to routes...!) 
 })
 
+app.get('/home, () => {
+    console.log("Welcome to home page")        
+})    
+   
 app.listen(3000, () => {
   console.log("Server listening on 3000")
 })
