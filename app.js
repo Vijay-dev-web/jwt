f71d618f839829dad0c50c4b25a352ccaa323870
sahayavijay <sahayavijay1993@gmail.com>
2022-10-31 22:09:25 +0530

    added error page

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ app.get('/', () => {
     console.log("Welcome to routes...!) 
 })
 
+app.get('/error', () => {
+    res.status(404).send("Page not found!!!")
+})    
+    
 app.listen(3000, () => {
   console.log("Server listening on 3000")
 })
